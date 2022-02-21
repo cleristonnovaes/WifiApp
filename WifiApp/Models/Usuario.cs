7f038cd3bd5b411ecc0faa924011add4e0f5125e
Cleriston <NOC-Cleriston@DESKTOP-LFMC7FD>
2022-02-21 12:44:58 -0300

    Adicionar arquivos de projeto.

---
+++ WifiApp/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WifiApp.Models
+{
+    public class Usuario
+    {
+        public int CPF { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ WifiApp/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WifiApp.ViewModels
+{
+    public class UsuarioViewModel
+    {
+    }
+}
